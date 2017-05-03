0531bea005504a6754c363639fa538d70e6b8ad2
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2017-05-03 14:17:38 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='eregs',
+    version='0.0.1',
+    author='Jerry Vinokurov and Chris Contolini',
+    author_email='yury.vinokurov@cfpb.gov',
+    packages=find_packages(),
+    url='https://github.com/cfpb/eregs-2.0',
+    license='MIT',
+    description='The new version of eRegs.',
+    include_package_data=True,
+)
