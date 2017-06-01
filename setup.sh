572b49027618e3e422360a1ed9c33f4bb059ea21
Chris Contolini <chris@contolini.com>
2017-06-01 13:19:52 -0400

    Set up Travis CI

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ set -e
 
 init(){
     NODE_DIR=node_modules
-
     echo 'npm components directory:', $NODE_DIR
 }
 
 # Clean project deps
 clean(){
     if [ -d $NODE_DIR ]; then
-	echo 'Removing project dependency directories...'
-	rm -rf $NODE_DIR
+      echo 'Removing project dependency directories...'
+      rm -rf $NODE_DIR
     fi
     echo 'Project dependencies have been removed.'
 }
@@ -23,12 +22,13 @@ clean(){
 install(){
     echo 'Installing project dependencies...'
     npm install
+    npm install -g grunt-cli
 }
 
 # Build
 build(){
     echo 'Building project...'
-    grunt build
+    npm run build
 }
 
 init
