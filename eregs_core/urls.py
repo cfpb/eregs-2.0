48b10dc23f1d5469e24a58e519721f0e377b5ed4
Andy Chosak <chosak@users.noreply.github.com>
2017-08-25 15:44:10 -0400

    convert hardcoded urls to dynamic ones
    This change removes usage of hardcoded URLs in favor of using Django's
    `django.core.urlresolvers.reverse` and `{% url %}` template tag to
    better support cases where this app is being deployed under a website's
    subdirectory.
    
    For example, if this app is deployed under a `/eregs-2.0` subdirectory,
    regulation URLs should be under `/eregs-2.0/regulation/etc`, and not
    under a hardcoded `/regulation/etc`.
    
    In addition, a couple of hardcoded `/static` references in templates
    have been converted to use of Django's `{% static %}` tag, for a similar
    reason.
    
    All existing unit tests continue to pass, and working site functionality
    continues to do so.

--- eregs_core/urls.py
+++ eregs_core/urls.py
@@ -7,24 +7,48 @@ from eregs_core.views import (
 
 
 urlpatterns = [
-    url(r'^$', main),
-    url(r'^(?P<part_number>[\d]{4})', regulation_main),
+    url(r'^$', main, name='eregs_main'),
+    url(r'^(?P<part_number>[\d]{4})',
+        regulation_main,
+        name='eregs_regulation_main'),
     url((
-        r'^regulation/'
-        '(?P<version>[\d]+-[\d]+)/'
-        '(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
-        '(?P<node>.*)$'
-    ), regulation),
-    url(r'^diff_redirect/(?P<left_version>[\d]+-[\d]+)/(?P<left_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/', diff_redirect),
-    url(r'^search/$', search),
-    url(r'^diff/(?P<left_version>[\d]+-[\d]+)/(?P<left_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
-        r'(?P<right_version>[\d]+-[\d]+)/(?P<right_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', diff),
+            r'^regulation/'
+            '(?P<version>[\d]+-[\d]+)/'
+            '(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
+            '(?P<node>.*)$'
+        ),
+        regulation,
+        name='eregs_regulation'),
+    url((
+            r'^diff_redirect/'
+            '(?P<left_version>[\d]+-[\d]+)/'
+            '(?P<left_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
+        ),
+        diff_redirect,
+        name='eregs_diff_redirect'),
+    url(r'^search/$', search, name='eregs_search'),
+    url((
+            r'^diff/(?P<left_version>[\d]+-[\d]+)/'
+            '(?P<left_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
+            '(?P<right_version>[\d]+-[\d]+)/'
+            '(?P<right_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
+            '(?P<node>.*)$'
+        ),
+        diff,
+        name='eregs_diff'),
     url(r'^partial/diff/(?P<left_version>[\d]+-[\d]+)/(?P<left_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
         r'(?P<right_version>[\d]+-[\d]+)/(?P<right_eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', diff_partial),
     url(r'^partial/(?P<version>[\d]+-[\d]+)/(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', regulation_partial),
-    url(r'^partial/sxs/(?P<version>[\d]+-[\d]+)/(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', sxs_partial),
+    url((
+        r'^partial/sxs/'
+        '(?P<version>[\d]+-[\d]+)/'
+        '(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/'
+        '(?P<node>.*)$'
+        ),
+        sxs_partial,
+        name='eregs_sxs_partial'),
     url(r'^partial/sidebar/(?P<version>[\d]+-[\d]+)/(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', sidebar_partial),
     url(r'^partial/definition/(?P<version>[\d]+-[\d]+)/(?P<eff_date>[\d]{4}-[\d]{2}-[\d]{2})/(?P<node>.*)$', definition_partial),
-    url(r'^partial/search/$', search_partial),
+    url(r'^partial/search/$', search_partial, name='eregs_partial_search'),
 ]
 
